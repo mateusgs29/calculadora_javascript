8dc3d5f5b2f6a9f54b44442dc024494b2a4f5b8d
Mateus Gomes <mateus.gs1999@gmail.com>
2020-11-19 14:13:21 -0300

    consertado precisão do calculo e adicionado bloqueio de botões não numéricos

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 let visor = document.querySelector(".visor");
 let expressao = '';
+let digitouNumero = false;
 
 function clicked(n){
-    expressao += n;
-    visor.innerHTML = expressao;
+    // se digitar um número, pode digitar a vontade
+    if(typeof(n) === "number"){
+        expressao += n;
+        visor.innerHTML = expressao;
+        digitouNumero = true;
+    // se não for um número, verifica se já foi digitado algum número
+    }else if(digitouNumero){ 
+        expressao += n;
+        visor.innerHTML = expressao;
+        digitouNumero = false;
+    }
 }
 
 function reset(){
+    // Botão de resetar o visor da calculadora
     expressao = '';
     visor.innerHTML = expressao;
+    digitouNumero = false;
 }
 
 function result(){
-    visor.innerHTML = eval(expressao);
-    expressao = '';
+    if(expressao){
+        let resultado = eval(expressao).toFixed(4); // eval faz o calculo de uma expressão string 
+        visor.innerHTML = Number(resultado); // transforma em Number para tirar os zeros a direita
+        expressao = '';
+        digitouNumero = false;
+    }
 }
